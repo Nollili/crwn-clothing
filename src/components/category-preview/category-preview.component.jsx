f4733f58124080ee440dfe2d64e104a66983ae40
noemi_verebelyi <noemi_verebelyi@epam.com>
2025-11-20 16:01:44 +0100

    fix some styles

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,11 @@
 import './category-preview.styles.scss';
 import ProductCard from '../product-card/product-card.component';
+import {Link} from 'react-router-dom';
 
 const CategoryPreview = ({title, products}) => {
   return (
     <div className="category-preview-container">
-      <h2 className="title"><span>{title.toUpperCase()}</span></h2>
+      <Link className="title" to={title}><span>{title.toUpperCase()}</span></Link>
       <div className="preview">
         {products
           .filter((_, idx) => idx < 4)
